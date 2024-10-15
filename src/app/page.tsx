fddbe8d0128529f335a8dd030152dbc26e743181
vikas kaundal <vikaskaundal21@gmail.com>
2024-10-15 18:59:21 +0530

    my-first-commit

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+// import { TestComponent } from "@/features/test";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Input/>  
+      <Button variant="primary" size="lg">
+        Primary
+      </Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritary</Button>
+
+      {/* <TestComponent></TestComponent> */}
+    </div>
+  );
+}
---
+++ src/features/test.tsx
@@ -0,0 +1,7 @@
+export const TestComponent = () =>{
+    return (
+        <div className="text-green-500 font-mono bg-orange-300">
+            Hello Test Component
+        </div>
+    )
+}
