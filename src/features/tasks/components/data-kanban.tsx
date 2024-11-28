cbd03fc764f7b43402a63ec76d8b7e8d78c0bb24
vikas kaundal <vikaskaundal21@gmail.com>
2024-11-28 20:05:58 +0530

    finish the kanban view status

--- src/features/tasks/components/data-kanban.tsx
+++ src/features/tasks/components/data-kanban.tsx
@@ -4,9 +4,10 @@ import{
 DragDropContext,
 Droppable,
 Draggable,
-DropResult,
+type DropResult,
 } from "@hello-pangea/dnd";
 
+import { KanbanCard } from "./kanban-card";
 import { KanbanColumnHeader } from "./kanban-column-header";
 
 import { Task, TaskStatus } from "../types";
@@ -25,10 +26,12 @@ type TaskState = {
 
 interface DataKanbanProps {
     data: Task[];
+    onChange: (tasks: { $id: string; status: TaskStatus; position: number} []) => void;
 };
 
-export const DataKanban = ( {
+export const DataKanban = ({
     data,
+    onChange,
 }: DataKanbanProps) => {
     const [tasks, setTasks] = useState<TaskState>(() => {
         const initialTasks: TaskState = {
@@ -50,8 +53,114 @@ export const DataKanban = ( {
         return initialTasks;
     });
 
+    useEffect(() => {
+        const newTasks: TaskState = {
+            [TaskStatus.BACKLOG]: [],
+            [TaskStatus.TODO]: [],
+            [TaskStatus.IN_PROGRESS]: [],
+            [TaskStatus.IN_REVIEW]: [],
+            [TaskStatus.DONE]: [],
+        };
+
+        data.forEach((task) => {
+            newTasks[task.status].push(task);
+        });
+
+        Object.keys(newTasks).forEach((status) => {
+            newTasks[status as TaskStatus].sort((a,b) => a.position - b.position);
+        });
+
+        setTasks(newTasks);
+
+    }, [data]); 
+
+    const onDragEnd = useCallback((result: DropResult) => {
+        if(!result.destination) return;
+
+
+        const  { source, destination } = result;
+        const sourceStatus = source.droppableId as TaskStatus;
+        const destStatus = destination.droppableId as TaskStatus;
+
+        let updatesPayload: { $id: string; status: TaskStatus; position: number; } [] = [];
+
+        setTasks((prevTasks) => {
+            const newTasks = { ...prevTasks };
+
+            // Safely remove the task from the source column
+            const sourceColumn = [...newTasks[sourceStatus]];
+            const [movedTask] = sourceColumn.splice(source.index, 1);
+
+            //  If there's is no moved task (shouldn't happen but just in case), return the previous state.
+           if(!movedTask) {
+            console.error("no task found at source index")
+            return prevTasks; 
+           }
+
+        // Create a new task object with potentially updated status.
+        const updatedMovedTask = sourceStatus !== destStatus
+           ? {...movedTask, status: destStatus }
+           : movedTask;
+
+        // Update the source column. 
+        newTasks[sourceStatus] = sourceColumn;
+
+
+        // Add the task to the new destination column
+        const  destColumn = [...newTasks[destStatus]];
+        destColumn.splice(destination.index, 0, updatedMovedTask);
+        newTasks[destStatus] = destColumn;
+
+        // Prepare minimal update payloads
+        updatesPayload = [];
+
+        // ALways update the moved task
+        updatesPayload.push({
+            $id: updatedMovedTask.$id,
+            status: destStatus,
+            position: Math.min((destination.index +1) * 1000, 1_000_000)
+        });
+
+        // Update position for affected task in the destination column
+        newTasks[destStatus].forEach((task, index) => {
+            if(task && task.$id !== updatedMovedTask.$id) {
+                const newPosition = Math.min((index + 1) * 1000, 1_000_000); 
+                if(task.position !== newPosition){
+                    updatesPayload.push({
+                        $id: task.$id,
+                        status: destStatus,
+                        position: newPosition,
+                    });
+                }
+            }
+        });
+
+
+        // if the task moved b/w columns,  update position in the source column
+        if(sourceStatus !== destStatus){
+            newTasks[sourceStatus].forEach((task, index) => {
+                if(task) {
+                    const newPosition = Math.min((index + 1) * 1000, 1_000_000);
+                    if(task.position !== newPosition) {
+                        updatesPayload.push({
+                            $id: task.$id,
+                            status: sourceStatus,
+                            position: newPosition,
+                        });
+                    }
+
+                }
+            });
+        }
+
+        return newTasks;
+        });
+
+        onChange(updatesPayload);
+    }, [onChange]);
+
     return (
-        <DragDropContext onDragEnd={() => {}}>
+        <DragDropContext onDragEnd={onDragEnd}>
            <div className="flex overflow-x-auto">
                 {boards.map((board) => {
                     return (
@@ -60,6 +169,34 @@ export const DataKanban = ( {
                             board={board}
                             taskCount={tasks[board].length}
                             />
+                            <Droppable droppableId={board}>
+                                {(provided) => (
+                                    <div
+                                    {...provided.droppableProps}
+                                    ref={provided.innerRef}
+                                    className="min-h-[200px] py-1.5"
+                                    >
+                                        {tasks[board].map((task, index) => (
+                                            <Draggable
+                                            key={task.$id}
+                                            draggableId={task.$id}
+                                            index={index}
+                                            >
+                                                {(provided) => (
+                                                    <div
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                    >
+                                                    <KanbanCard task={task} />
+                                                    </div>
+                                                )}
+                                            </Draggable>
+                                        ))}
+                                        {provided.placeholder}
+                                    </div>
+                                )}
+                            </Droppable>
                         </div>
                     )
                 })}
--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useCallback } from "react";
 import { Loader, PlusIcon } from "lucide-react";
 import { useQueryState } from "nuqs";
 
@@ -13,11 +14,14 @@ import { DataFilters } from "./data-filters";
 
 import { columns } from "./column";
 import { DataTable } from "./data-table";
+import { DataKanban } from "./data-kanban";
 
+import { TaskStatus } from "../types";
 import { useGetTasks } from "../api/use-get-tasks";
 import { useCreateTaskModal } from "../hooks/use-create-task-modal";
 import { useTaskFilters } from "../hooks/use-task-filters";
-import { DataKanban } from "./data-kanban";
+import { useBulkUpdateTasks } from "../api/use-bulk-update-tasks";
+
 
 
 
@@ -36,6 +40,9 @@ export const TaskViewSwitcher = () => {
 
     const workspaceId = useWorkspaceId();
     const { open } = useCreateTaskModal();
+
+    const { mutate: bulkUpdate } = useBulkUpdateTasks(); 
+
     const { 
         data: tasks, 
         isLoading: IsLoadingTasks 
@@ -46,6 +53,14 @@ export const TaskViewSwitcher = () => {
         assigneeId,
         dueDate,
      });
+
+     const onKanbanChange = useCallback ((
+        tasks: { $id: string; status: TaskStatus; position: number }[]
+     ) => {
+        bulkUpdate({
+            json: {tasks},
+        });
+     }, [bulkUpdate]);
 
     return(
         <Tabs
@@ -98,7 +113,7 @@ export const TaskViewSwitcher = () => {
                <DataTable columns={columns} data={tasks?.documents ?? []}/>
             </TabsContent>
             <TabsContent value="kanban" className="mt-0">
-             <DataKanban data={tasks?.documents ?? []}/>
+             <DataKanban onChange={onKanbanChange} data={tasks?.documents ?? []}/>
             </TabsContent>
             <TabsContent value="calendar" className="mt-0">
              {JSON.stringify(tasks)}
